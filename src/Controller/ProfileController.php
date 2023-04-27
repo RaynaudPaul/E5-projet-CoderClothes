709884913d3b7452c7cd33a4340e9054cef47ddf
RaynaudPaul <raynaud.pau@educ-valadon-limoges.fr>
2023-04-27 16:11:36 +0200

    Update !!!!

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,59 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileFormType;
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 #[Route('/profil', name: 'app_profile_')]
 class ProfileController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(Request $request,UsersRepository $usersRepository, TokenStorageInterface $tokenStorage,EntityManagerInterface $entityManager): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'Profile de l\'utilisateur',
-        ]);
+        //$user = $authenticationUtils->getLastUsername();
+
+        $token = $tokenStorage->getToken();
+
+        // Vérifier si un utilisateur est connecté
+        if ($token && $token->getUser()) {
+            // Récupérer l'objet User
+            $user = $token->getUser();
+
+            //dd($user);
+
+            $form = $this->createForm(ProfileFormType::class, $user);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // encode the plain password
+                /*$user->setPassword(
+                    $userPasswordHasher->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );*/
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+                // do anything else you need here, like send an email
+
+
+            }
+
+            return $this->render('profile/index.html.twig', [
+                'controller_name' => 'Profile de l\'utilisateur',
+                'user' => $user,
+                'profileForm' => $form->createView(),
+            ]);
+        }
+
+
     }
 
     #[Route('/commandes', name: 'orders')]
